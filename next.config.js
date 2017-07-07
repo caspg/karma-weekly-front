238233339200ae869c9960aeeba45db3d6d190f9
Kacper Golinski <kacper.golinski@gmail.com>
2017-07-07 12:00:59 +0200

    add custom web pack config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config(); // eslint-disable-line global-require
+}
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.GRAPHQL_URI': JSON.stringify(process.env.GRAPHQL_URI),
+      })
+    );
+
+    return config;
+  },
+};
