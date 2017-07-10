541c60825d01faea16cfae23a8f5bf39efcfaa2b
Kacper Golinski <kacper.golinski@gmail.com>
2017-07-10 18:54:03 +0200

    crate jwtService

---
+++ src/services/jwtService/jwtService.js
@@ -0,0 +1,30 @@
+const ITEM_KEY = 'karma_jwt';
+
+/**
+ * Save jwToken in localStorage.
+ * @param {string} jwToken
+ */
+function saveInLocal(jwToken) {
+  localStorage.setItem(ITEM_KEY, jwToken);
+}
+
+/**
+ * Get jwToken from the localStorage.
+ * @returns {string} jwToken
+ */
+function getFromLocal() {
+  return localStorage.getItem(ITEM_KEY);
+}
+
+/**
+ * Remove jwToken from the localStorage.
+ */
+function removeFromLocal() {
+  localStorage.removeItem(ITEM_KEY);
+}
+
+export default {
+  saveInLocal,
+  getFromLocal,
+  removeFromLocal,
+};
---
+++ src/services/jwtService/jwtService.test.js
@@ -0,0 +1,40 @@
+import jwtService from '../jwtService';
+
+describe('jwtService', () => {
+  const setItemMock = jest.fn();
+  const getItemMock = jest.fn();
+  const removeItemMock = jest.fn();
+
+  const localStorageMock = {
+    setItem: (key, value) => setItemMock(key, value),
+    getItem: key => getItemMock(key),
+    removeItem: key => removeItemMock(key),
+  };
+
+  beforeAll(() => {
+    global.localStorage = localStorageMock;
+  });
+
+  beforeEach(() => { jest.resetAllMocks(); });
+
+  const expectedTokenKey = 'karma_jwt';
+  const jwToken = 'some-long-well-encrypted-token';
+
+  test('saveInLocal - it sets item in the localStorage', () => {
+    jwtService.saveInLocal(jwToken);
+
+    expect(setItemMock).toHaveBeenCalledWith(expectedTokenKey, jwToken);
+  });
+
+  test('getFromLocal - it gets item from the localStorage', () => {
+    jwtService.getFromLocal();
+
+    expect(getItemMock).toHaveBeenCalledWith(expectedTokenKey);
+  });
+
+  test('removeFromLocal - it removes item from the localStorage', () => {
+    jwtService.removeFromLocal();
+
+    expect(removeItemMock).toHaveBeenCalledWith(expectedTokenKey);
+  });
+});
